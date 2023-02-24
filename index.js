c1bdc678c5c5b955d13c1aec5cd4c50ea2a5081c
madeline <madeline.j.dick@gmail.com>
2023-02-23 18:08:02 -0600

    overcomplicated things, then simplified. facepalm.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+const random = Math.round(Math.random() + 1)
+const num3 = 16
+const num4 = 6
+const mod = num3 % num4
+const num5 = 17
+const num6 = 20
+const max = Math.max(num1, num3, num4, num5, num6)
